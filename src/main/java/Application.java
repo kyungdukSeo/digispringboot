3ffd9cf4fc306c2225b676f2ba95adbf83530b29
kyungdukSeo <skd0220@gmail.com>
2022-04-20 11:01:21 +0900

    20220420_10:48

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // Alt + Enter = import (Ctrl + Shift + o)
 @SpringBootApplication
+// SpringBoot 를 자동 설정
+// 여기서부터 설정을 읽는다.
+// 항상 프로젝트의 최상단에 위치
+
 public class Application
 {
     public static void main(String[] args)
