98a0142e056f4f03a78b79cb7e5ed204c6de36fb
kyungdukSeo <skd0220@gmail.com>
2022-04-20 11:03:29 +0900

    20220420_11:03

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -15,6 +15,8 @@ public class Application
     public static void main(String[] args)
     {
         SpringApplication.run(Application.class, args);
+        // main 에서 시작
+        // 내장 WAS(Web Application Server) 실행
 
     }
 }
