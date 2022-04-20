86f81b8074ba8a124edf44ebbe4c9ddcfbafca0a
kyungdukSeo <skd0220@gmail.com>
2022-04-20 15:11:23 +0900

    20220420_11:03

--- src/test/java/com/kbstar/springboot/study/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/kbstar/springboot/study/web/dto/HelloResponseDtoTest.java
@@ -1,4 +1,25 @@
 package com.kbstar.springboot.study.web.dto;
 
-public class HelloResponseDtoTest {
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+/*
+    06. Getter가 자동으로 동작하는지 단위테스트
+
+ */
+public class HelloResponseDtoTest
+{
+    @Test
+    public void lombokTest()
+    {
+        String name = "test";
+        int age = 34;
+
+        HelloResponseDto dto = new HelloResponseDto(name, age);
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAge()).isEqualTo(age);
+    }
 }
