806b12d2257310e051ab91e96c058dc2341eb149
kyungdukSeo <skd0220@gmail.com>
2022-04-20 10:49:05 +0900

    20220420_10:48

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,16 @@
 
 // com.kbstar.study
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Alt + Enter = import (Ctrl + Shift + o)
+@SpringBootApplication
 public class Application
 {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Application.class, args);
 
+    }
 }
