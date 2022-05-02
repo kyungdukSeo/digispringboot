a23dcd03504363f1bd1e649ab07b4c31fbf41b22
kyungdukSeo <skd0220@gmail.com>
2022-05-02 15:06:05 +0900

    20220502

--- src/main/java/com/kbstar/springboot/study/web/IndexController.java
+++ src/main/java/com/kbstar/springboot/study/web/IndexController.java
@@ -62,7 +62,7 @@ public class IndexController
         SessionUser user = (SessionUser)httpSession.getAttribute("user");
         if(user != null)
         {
-            model.addAttribute("userName", user.getName());
+            model.addAttribute("kbUserName", user.getName());
         }
 
 
